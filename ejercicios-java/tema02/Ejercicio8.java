90f6c199111c537a5bb7ed8da5a49e93576c905e
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-10-14 11:05:03 +0200

    terminados

---
+++ ejercicios-java/tema02/Ejercicio8.java
@@ -0,0 +1,18 @@
+/**
+ * Fecha 14/10/2021
+ *
+ * @author Francisco J. Sicilia
+ */
+public class Ejercicio8 {
+  public static void main(String[] args) {
+    
+    char letra0 = 'A';
+    char letra = 'B';
+    char letra1 = 'C';
+    char letra2 = 'D';
+    char letra3 = 'E';
+    char letra4 = 'F';
+    String letras = ""+letra0+ letra +letra1 +letra2 +letra3 +letra4;
+    System.out.println("Letras: " + letras);
+  }
+}
