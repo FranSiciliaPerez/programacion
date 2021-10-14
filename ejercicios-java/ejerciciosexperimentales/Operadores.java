160af6384484e99396fab10623a9823d44a86ff8
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-10-14 09:32:06 +0200

    Nuevo ejercicio

---
+++ ejercicios-java/ejerciciosexperimentales/Operadores.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @author Francisco J. Sicilia
+ */
+public class Operadores {
+  public static void main(String[] args) {
+    
+    String elTiempoDeHoy = "Lluvioso";
+    int x = 3;
+    
+    int suma = ++x;
+    System.out.println(suma);
+  }
+}
