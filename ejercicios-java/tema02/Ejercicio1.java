5a3197c2d64c9d6130abdc460df441c2028e4621
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-10-14 10:02:12 +0200

    Nuevo tema(tema2)

---
+++ ejercicios-java/tema02/Ejercicio1.java
@@ -0,0 +1,13 @@
+/**
+ * Fecha 14/10/2021
+ *
+ * @author Francisco J. Sicilia
+ */
+public class Ejercicio1 {
+  public static void main(String[] args) {
+    int x = 144;
+    int y = 999;
+    int suma = x + y; 
+    System.out.println("La suma de x + y es: " + suma);
+  }
+}
