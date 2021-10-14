4c32f9127686d181cb494a974a0c8f07ab292a7f
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-10-14 10:42:08 +0200

    Ejercicio7

---
+++ ejercicios-java/tema02/Ejercicio7.java
@@ -0,0 +1,14 @@
+/**
+ * Fecha 14/10/2021
+ *
+ * @author Francisco J. Sicilia
+ */
+public class Ejercicio7 {
+  public static void main(String[] args) {
+    
+    char letra = 'A';
+    String nombre = "Francisco";
+    
+    System.out.println("La letra 'A'" + letra + " de Fran " + nombre);
+  }
+}
