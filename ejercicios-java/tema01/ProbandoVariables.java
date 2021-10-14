3597b0d1b59b480b81f3a2f6c5a358471c566f8d
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-10-14 09:08:33 +0200

    Actualización ejercicios

---
+++ ejercicios-java/tema01/ProbandoVariables.java
@@ -0,0 +1,13 @@
+/**
+ * Uso de variables que contienen números decimales
+ *
+ * @author Francisco J. Sicilia
+ */
+public class ProbandoVariables {
+  public static void main(String[] args) {
+    long numero = 1234567891011l;
+    double numero1 = 12.9999d;
+    System.out.println(numero);
+    System.out.println(numero1);
+  }
+}
