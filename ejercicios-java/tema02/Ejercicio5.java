67593a1e7945a76bf4ef2afad3efd3d480c043f7
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-10-14 10:26:47 +0200

    Ejercicio5

---
+++ ejercicios-java/tema02/Ejercicio5.java
@@ -0,0 +1,14 @@
+/**
+ * Fecha 14/10/2021
+ *
+ * @author Francisco J. Sicilia
+ */
+public class Ejercicio5 {
+  public static void main(String[] args) {
+    
+    double pesetas = 1000;
+    double pesetasAEuros = pesetas * 0.006;
+    
+    System.out.println("1000 pesetas son: " + pesetasAEuros + " euros" );
+  }
+}
