70ed31a9366d26a58948704c2b28aea3c021d62e
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-10-14 10:35:45 +0200

    Ejercicio6

---
+++ ejercicios-java/tema02/Ejercicio6.java
@@ -0,0 +1,15 @@
+/**
+ * Fecha 14/10/2021
+ *
+ * @author Francisco J. Sicilia
+ */
+public class Ejercicio6 {
+  public static void main(String[] args) {
+    
+    double iva = 1.21;
+    double precioSinIva = 20;
+    double preciofinal = precioSinIva * iva;
+    
+    System.out.println("El precio sin iva es: " + preciofinal );
+  }
+}
