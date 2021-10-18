0454a57f28a2c310bed89c1f3bbfe5b361fbacd5
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-10-18 09:47:00 +0200

    Nuevo tema(Tema3)

---
+++ ejercicios-java/tema03/Ejercicio1.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author Francisco J. Sicilia
+ */
+public class Ejercicio1 {
+  public static void main(String[] args) {
+  
+  System.out.println("Introduce el nobre de una fruta: ");
+  String fruta = System.console().readLine();
+  System.out.println("El nombre intrducid es: " + fruta);
+   
+  }
+}
