bf1682092bc60af13ec45f9add8d8bd23d15621e
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-10-18 10:12:23 +0200

    Ejercicios

--- ejercicios-java/ejerciciosexperimentales/ProbandoLecturaDeNumeros.java
+++ ejercicios-java/ejerciciosexperimentales/ProbandoLecturaDeNumeros.java
@@ -5,11 +5,14 @@
 public class ProbandoLecturaDeNumeros {
   public static void main(String[] args) {
   
-  System.out.println("Introduce el nobre de una fruta: ");
-   
+  System.out.println("Introduce un numero: ");
   int primerNumero = Integer.parseInt(System.console().readLine());
+  
+  System.out.println("Introduce el nobre de una fruta: ");
+  String fruta = System.console().readLine();
 
   System.out.println("El nombre intrducido es: " + primerNumero);
+  System.out.println("La fruta intrducida es: " + fruta);
    
   }
 }
