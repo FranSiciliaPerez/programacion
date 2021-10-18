222ce0b0dde663942ceb2d85341ed3ae42654ebc
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-10-18 10:18:20 +0200

    ejercicio1 tema 3

--- ejercicios-java/tema03/Ejercicio1.java
+++ ejercicios-java/tema03/Ejercicio1.java
@@ -5,11 +5,16 @@
 public class Ejercicio1 {
   public static void main(String[] args) {
   
-  System.out.println("Introduce el nobre de una fruta: ");
-   
+  System.out.println("--A continuación introduce valores de los números que se van a sumar--");
+  System.out.printf("Introduce el primer numero: ");
   int primerNumero = Integer.parseInt(System.console().readLine());
-
-  System.out.println("El nombre intrducido es: " + primerNumero);
+  
+  System.out.printf("Introduce el primer numero: ");
+  int segundoNumero = Integer.parseInt(System.console().readLine());
+  
+  int suma = primerNumero + segundoNumero;
+  
+  System.out.println("La suma de los dos números introducidos es: " + suma);
    
   }
 }
