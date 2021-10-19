9e455ed5fe090d24003bb7d7847e7b84f5461b84
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-10-19 09:25:22 +0200

    Ej 7 tema 03

---
+++ ejercicios-java/tema03/Ejercicio7.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * @author Francisco J. Sicilia
+ */
+public class Ejercicio7 {
+  public static void main(String[] args) {
+  
+    System.out.println("--Calcula el total de una factura a partir de la base imponible--");
+    System.out.printf("Introduce el IVA: ");
+    double iva = Double.parseDouble(System.console().readLine());
+  
+    System.out.printf("Introduce el precio del articulo: ");
+    double precioSinIva = Double.parseDouble(System.console().readLine());
+  
+    double preciofinal = precioSinIva * iva;
+    
+    
+    System.out.println("El precio sin iva es: " + precioSinIva + " euros");
+    System.out.println("El IVA es: " + iva + " euros" );
+    System.out.println("El precio final es: " + preciofinal + " euros");
+  
+  }
+}
