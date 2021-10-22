e1efa37bac6be414ff3a1a200aaefebad9218d43
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-10-22 21:49:28 +0200

    Ejercicio2 tema04

---
+++ ejercicios-java/tema04/Ejercicio2.java
@@ -0,0 +1,28 @@
+/**
+ * Realiza un programa que pida una hora por teclado y que muestre luego buenos
+ * días, buenas tardes o buenas noches según la hora. Se utilizarán los tramos de
+ * 6 a 12, de 13 a 20 y de 21 a 5. respectivamente. Sólo se tienen en cuenta las
+ * horas, los minutos no se deben introducir por teclado.
+ * 
+ * @author Francisco J. Sicilia
+ */
+public class Ejercicio2 {
+  public static void main(String[] args) {
+  
+System.out.println("A continuación escriba la hora actual");
+    int horaDelDia = Integer.parseInt(System.console().readLine());
+    
+    if ((horaDelDia > 5) && (horaDelDia < 12)) {
+      System.out.println("Buenos dias");
+    }
+    if ((horaDelDia > 11) && (horaDelDia < 21)) {
+        System.out.println("Buenas tardes");
+    }
+    if ((horaDelDia > 20) || (horaDelDia < 6)) {
+          System.out.println("Buenas noches");
+    } 
+    
+    
+    
+  }
+}
