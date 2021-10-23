5c6ec132f69287d810362629bf3f99dbfa93ee53
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-10-23 14:04:12 +0200

    Actualizacion

--- ejercicios-java/tema04/Ejercicio6.java
+++ ejercicios-java/tema04/Ejercicio6.java
@@ -1,1 +1,19 @@
+/**
+ * Realiza un programa que calcule el tiempo que tardará en caer un objeto desde
+ * una altura h. Aplica la fórmula t =√2h/g siendo g = 9:81m/s2
+ * 
+ * @author Francisco J. Sicilia
+ */
+public class Ejercicio6 {
+  public static void main(String[] args) {
+  
+    System.out.printf("Por favor, introduzca el valor de la altura(en metros): ");
+    double h = Double.parseDouble(System.console().readLine());
+    
+    double tiempo = Math.sqrt((2 * h)/9.81);
+    System.out.println("El tiempo que tarda el objeto en caer desde " + h + " es " + tiempo + " segundos");
+   
+  }
+}
+
 
