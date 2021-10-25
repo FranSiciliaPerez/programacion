210c5436b365f33423bfca06dcf92eb3b822e4ca
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-10-25 09:38:57 +0200

    actualizacion

---
+++ ejercicios-java/tema04/Ejercicio11.java
@@ -0,0 +1,22 @@
+/**
+ * Realiza un programa que calcule la media de tres notas
+ * 
+ * @author Francisco J. Sicilia
+ */
+public class Ejercicio11 {
+  public static void main(String[] args) {
+  
+    System.out.println("A continuación introduce una hora determinada");
+    System.out.printf("Por favor, introduzca la hora: ");
+    double horas = Double.parseDouble(System.console().readLine());
+    
+    System.out.printf("Por favor, introduzca los minutos: ");
+    double minutos = Double.parseDouble(System.console().readLine());
+    
+    
+    double notaMedia = (nota1 + nota2 + nota3) / 3;
+    
+   
+  }
+}
+
