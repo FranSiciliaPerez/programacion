b548ae3b59789a689fb2f9d74f04acd89a821b29
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-10-26 12:24:20 +0200

    ejercicio12 tema04

--- ejercicios-java/tema04/Ejercicio12.java
+++ ejercicios-java/tema04/Ejercicio12.java
@@ -6,17 +6,21 @@
 public class Ejercicio12 {
   public static void main(String[] args) {
   
-    System.out.println("A continuación introduce una hora determinada");
+    System.out.println("A continuación vas a proceder a realizar un questionario tipo test de 10 preguntas sobre las asignaturas que se imparten en el curso: ");
     System.out.printf("Por favor, introduzca la hora: ");
-    double horas = Double.parseDouble(System.console().readLine());
+    String respuesta1 = System.console().readLine();
     
-    System.out.printf("Por favor, introduzca los minutos: ");
-    double minutos = Double.parseDouble(System.console().readLine());
+    switch (respuesta1) {
+       case "a":
+       case "A":
+        int primeraPregunta = 1;
+       int resultadoExamen = primeraPregunta + 1;
+       System.out.println("Has sacado un: " + resultadoExamen + " en el examen");
+       break;
+       
+    }
     
-    double horasASegundos = (horas * 3600) + (minutos * 60);
-    double segundosRestantes  = (86400 - horasASegundos); 
     
-    System.out.printf("Segun las horas y minutos introducidos, faltan: " +segundosRestantes);
     
    
   }
