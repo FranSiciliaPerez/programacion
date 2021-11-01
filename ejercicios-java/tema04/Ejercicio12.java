bca745a3ba972d9dae61757f2ff404ed1a45a9c1
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-01 10:31:40 +0100

    Ejercicio12

--- ejercicios-java/tema04/Ejercicio12.java
+++ ejercicios-java/tema04/Ejercicio12.java
@@ -5,105 +5,109 @@
  */
 public class Ejercicio12 {
   public static void main(String[] args) {
-  
+    
+    int resultado1;
+    int resultado2;
+    int resultado3;
+    int resultado4;
+    int resultado5;
+    int resultado6;
+    int resultado7;
+    int resultado8;
+    int resultado9;
+    int resultado10;
     System.out.println("A continuación vas a proceder a realizar un questionario tipo test de 10 preguntas sobre las asignaturas que se imparten en el curso: ");
     System.out.printf("Primera pregunta: ¿Que lenguaje de los siguientes se utiliza en LM? ");
     System.out.printf("a-  ");
     System.out.printf("b-  ");
     System.out.printf("c-  ");
     String respuesta1 = System.console().readLine();
+    if ((respuesta1.equals ("a")) || (respuesta1.equals ("A"))) {
+      resultado1 = 1;
+    }
     
     System.out.printf("Primera pregunta: ¿Que lenguaje de los siguientes se utiliza en LM? ");
     System.out.printf("a-  ");
     System.out.printf("b-  ");
     System.out.printf("c-  ");
     String respuesta2 = System.console().readLine();
+    if ((respuesta2.equals ("b")) || (respuesta2.equals ("B"))) {
+      resultado2 = (resultado1 + 1);
+    }
+    
     
     System.out.printf("Primera pregunta: ¿Que lenguaje de los siguientes se utiliza en LM? ");
     System.out.printf("a-  ");
     System.out.printf("b-  ");
     System.out.printf("c-  ");
     String respuesta3 = System.console().readLine();
+    if ((respuesta3.equals ("c")) || (respuesta3.equals ("C"))) {
+      resultado3 = resultado2 + 1;
+    }
+    
     
     System.out.printf("Primera pregunta: ¿Que lenguaje de los siguientes se utiliza en LM? ");
     System.out.printf("a-  ");
     System.out.printf("b-  ");
     System.out.printf("c-  ");
     String respuesta4 = System.console().readLine();
+    if ((respuesta4.equals ("b")) || (respuesta4.equals ("B"))) {
+      resultado4 = resultado3 + 1;
+    }
     
     System.out.printf("Primera pregunta: ¿Que lenguaje de los siguientes se utiliza en LM? ");
     System.out.printf("a-  ");
     System.out.printf("b-  ");
     System.out.printf("c-  ");
     String respuesta5 = System.console().readLine();
+    if ((respuesta5.equals ("b")) || (respuesta5.equals ("B"))) {
+      resultado5 = resultado3 + 1;
+    }
     
     System.out.printf("Primera pregunta: ¿Que lenguaje de los siguientes se utiliza en LM? ");
     System.out.printf("a-  ");
     System.out.printf("b-  ");
     System.out.printf("c-  ");
     String respuesta6 = System.console().readLine();
+    if ((respuesta6.equals ("a")) || (respuesta6.equals ("A"))) {
+      resultado6 = 1;
+    }
     
     System.out.printf("Primera pregunta: ¿Que lenguaje de los siguientes se utiliza en LM? ");
     System.out.printf("a-  ");
     System.out.printf("b-  ");
     System.out.printf("c-  ");
     String respuesta7 = System.console().readLine();
+    if ((respuesta7.equals ("b")) || (respuesta7.equals ("B"))) {
+      resultado7 = resultado6 + 1;
+    }
     
     System.out.printf("Primera pregunta: ¿Que lenguaje de los siguientes se utiliza en LM? ");
     System.out.printf("a-  ");
     System.out.printf("b-  ");
     System.out.printf("c-  ");
     String respuesta8 = System.console().readLine();
+    if ((respuesta8.equals ("c")) || (respuesta8.equals ("C"))) {
+      resultado8 = resultado2 + 1;
+    }
     
     System.out.printf("Primera pregunta: ¿Que lenguaje de los siguientes se utiliza en LM? ");
     System.out.printf("a-  ");
     System.out.printf("b-  ");
     System.out.printf("c-  ");
     String respuesta9 = System.console().readLine();
+    if ((respuesta9.equals ("b")) || (respuesta9.equals ("B"))) {
+      resultado9 = resultado3 + 1;
+    }
     
     System.out.printf("Primera pregunta: ¿Que lenguaje de los siguientes se utiliza en LM? ");
     System.out.printf("a-  ");
     System.out.printf("b-  ");
     System.out.printf("c-  ");
     String respuesta10 = System.console().readLine();
-    
-    /*if ((respuesta1.equals ("a")) || (respuesta1.equals ("A"))) {
-      int resultado1 = 1;
+    if ((respuesta10.equals ("b")) || (respuesta9.equals ("B"))) {
+      resultado10 = resultado9 + 1;
     }
-    if ((respuesta2.equals ("b")) || (respuesta2.equals ("B"))) {
-      int resultado2 = resultado1 + 1;
-    }
-    if ((respuesta3.equals ("c")) || (respuesta3.equals ("C"))) {
-      int resultado3 = resultado2 + 1;
-    }
-    if ((respuesta4.equals ("b")) || (respuesta4.equals ("B"))) {
-      int resultado4 = resultado3 + 1;
-    }
-    if ((respuesta5.equals ("b")) || (respuesta5.equals ("B"))) {
-      int resultado5 = resultado3 + 1;
-    }
-     if ((respuesta6.equals ("a")) || (respuesta6.equals ("A"))) {
-      int resultado6 = 1;
-    }
-    if ((respuesta7.equals ("b")) || (respuesta7.equals ("B"))) {
-      int resultado7 = resultado + 1;
-    }
-    if ((respuesta8.equals ("c")) || (respuesta8.equals ("C"))) {
-      int resultado8 = resultado2 + 1;
-    }
-    if ((respuesta9.equals ("b")) || (respuesta9.equals ("B"))) {
-      int resultado9 = resultado3 + 1;
-    }
-   */
-   switch (respuesta1) {
-      case "a":
-      case "A":
-        int resultado = 0 + 1;
-        break;
-      default: 
-    }
-   
-   System.out.println("es: "+resultado);
    
    
   }
