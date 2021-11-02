ffebfce278c345f87a50b33821252a65ec546bea
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-02 10:15:23 +0100

    Ejercicio12

--- ejercicios-java/tema04/Ejercicio12.java
+++ ejercicios-java/tema04/Ejercicio12.java
@@ -10,107 +10,100 @@
 public class Ejercicio12 {
   public static void main(String[] args) {
     
-    int resultado1;
+    int resultado = 0;
     System.out.println("A continuación vas a proceder a realizar un questionario tipo test de 10 preguntas sobre las asignaturas que se imparten en el curso: ");
     System.out.printf("Primera pregunta: ¿Que lenguaje de los siguientes se utiliza en LM? ");
     System.out.printf("a-  ");
     System.out.printf("b-  ");
     System.out.printf("c-  ");
-    String respuesta1 = System.console().readLine();
-    if ((respuesta1.equals ("a")) || (respuesta1.equals ("A"))) {
-      resultado1 = 1;
+    String respuesta = System.console().readLine().toLowerCase();
+    /*resultado = respuesta.equals("a") ? resultado + 1 : resultado;
+    ++       sistema parecido*/
+    
+    
+    if (respuesta.equals ("a")) {
+      resultado++;
     }
     
-    int resultado2;
     System.out.printf("Segunda pregunta: ¿Que lenguaje de los siguientes se utiliza en LM? ");
     System.out.printf("a-  ");
     System.out.printf("b-  ");
     System.out.printf("c-  ");
-    String respuesta2 = System.console().readLine();
-    if ((respuesta2.equals ("b")) || (respuesta2.equals ("B"))) {
-      resultado2 = (resultado1 + 1);
+    respuesta = System.console().readLine();
+    if (respuesta.equals ("b")) {
+      resultado++;
     }
     
-    int resultado3;
     System.out.printf("Tercera pregunta: ¿Que lenguaje de los siguientes se utiliza en LM? ");
     System.out.printf("a-  ");
     System.out.printf("b-  ");
     System.out.printf("c-  ");
-    String respuesta3 = System.console().readLine();
-    if ((respuesta3.equals ("c")) || (respuesta3.equals ("C"))) {
-      resultado3 = resultado2 + 1;
+    respuesta = System.console().readLine();
+    if (respuesta.equals ("c")) {
+      resultado++;
     }
     
-    int resultado4;
     System.out.printf("Cuarta pregunta: ¿Que lenguaje de los siguientes se utiliza en LM? ");
     System.out.printf("a-  ");
     System.out.printf("b-  ");
     System.out.printf("c-  ");
-    String respuesta4 = System.console().readLine();
-    if ((respuesta4.equals ("b")) || (respuesta4.equals ("B"))) {
-      resultado4 = resultado3 + 1;
+    respuesta = System.console().readLine();
+    if (respuesta.equals ("b")) {
+      resultado++;
     }
-    int resultado5;
+
     System.out.printf("Quinta pregunta: ¿Que lenguaje de los siguientes se utiliza en LM? ");
     System.out.printf("a-  ");
     System.out.printf("b-  ");
     System.out.printf("c-  ");
-    String respuesta5 = System.console().readLine();
-    if ((respuesta5.equals ("b")) || (respuesta5.equals ("B"))) {
-      resultado5 = resultado3 + 1;
+    respuesta = System.console().readLine();
+    if (respuesta.equals ("b")) {
+      resultado++;
     }
     
-    int resultado6;
     System.out.printf("Sexta pregunta: ¿Que lenguaje de los siguientes se utiliza en LM? ");
     System.out.printf("a-  ");
     System.out.printf("b-  ");
     System.out.printf("c-  ");
-    String respuesta6 = System.console().readLine();
-    if ((respuesta6.equals ("a")) || (respuesta6.equals ("A"))) {
-      resultado6 = 1;
+    respuesta = System.console().readLine();
+    if (respuesta.equals ("a")) {
+      resultado++;
     }
     
-    
-    int resultado7;
     System.out.printf("Séptima pregunta: ¿Que lenguaje de los siguientes se utiliza en LM? ");
     System.out.printf("a-  ");
     System.out.printf("b-  ");
     System.out.printf("c-  ");
-    String respuesta7 = System.console().readLine();
-    if ((respuesta7.equals ("b")) || (respuesta7.equals ("B"))) {
-      resultado7 = resultado6 + 1;
+    respuesta = System.console().readLine();
+    if (respuesta.equals ("b"))  {
+      resultado++;
     }
     
-    int resultado8;
     System.out.printf("Octaba pregunta: ¿Que lenguaje de los siguientes se utiliza en LM? ");
     System.out.printf("a-  ");
     System.out.printf("b-  ");
     System.out.printf("c-  ");
-    String respuesta8 = System.console().readLine();
-    if ((respuesta8.equals ("c")) || (respuesta8.equals ("C"))) {
-      resultado8 = resultado2 + 1;
+    respuesta = System.console().readLine();
+    if (respuesta.equals ("c")) {
+      resultado++;
     }
     
-    
-    int resultado9;
     System.out.printf("Novena pregunta: ¿Que lenguaje de los siguientes se utiliza en LM? ");
     System.out.printf("a-  ");
     System.out.printf("b-  ");
     System.out.printf("c-  ");
-    String respuesta9 = System.console().readLine();
-    if ((respuesta9.equals ("b")) || (respuesta9.equals ("B"))) {
-      resultado9 = resultado3 + 1;
+    respuesta = System.console().readLine();
+    if ((respuesta.equals ("b")) || (respuesta.equals ("B"))) {
+      resultado++;
     }
     
-    
-    int resultado10;
     System.out.printf("Décima pregunta: ¿Que lenguaje de los siguientes se utiliza en LM? ");
     System.out.printf("a-  ");
     System.out.printf("b-  ");
     System.out.printf("c-  ");
-    String respuesta10 = System.console().readLine();
-    if ((respuesta10.equals ("b")) || (respuesta9.equals ("B"))) {
-      resultado10 = resultado9 + 1;
+    respuesta = System.console().readLine();
+    if ((respuesta.equals ("b")) || (respuesta.equals ("B"))) {
+      resultado++;
     }
    
    
