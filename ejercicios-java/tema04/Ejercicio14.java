5fb348e99e04e8f3d38eb5911f22a3c6dd827076
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-03 17:17:41 +0100

    Ejercicio14 tema04

--- ejercicios-java/tema04/Ejercicio14.java
+++ ejercicios-java/tema04/Ejercicio14.java
@@ -8,19 +8,19 @@ public class Ejercicio14 {
   public static void main(String[] args) {
     
     
-    System.out.println("Introduce un número");
+    System.out.print("Introduce un número: ");
     int a = Integer.parseInt(System.console().readLine());
     
     if ((a % 2) == (0)) {
-      System.out.println("Par");
+      System.out.print("El número introducido es par");
     } else {
-          System.out.println("Introduce un número");
+          System.out.print("El número introducido es impar");
     }
    
-    if ((a / 5) == (0)) {
-      System.out.println("Par");
+    if ((a % 5) == (0)) {
+      System.out.println(" y divisible entre 5");
     } else {
-          System.out.println("Introduce un número");
+          System.out.println(" y no es divisible entre 5");
     }
    
     
