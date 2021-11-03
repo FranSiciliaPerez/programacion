c6170fc17150a53d92c82e43124c63589af00ceb
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-03 19:03:48 +0100

    Ejercicio18 tema04

--- ejercicios-java/tema04/Ejercicio18.java
+++ ejercicios-java/tema04/Ejercicio18.java
@@ -5,8 +5,15 @@
  */
 public class Ejercicio18 {
   public static void main(String[] args) {
-    
    
+   int primera , n = 0; 
+   
+   System.out.print("Introduce un número de 5 cifras max y diré cual es la primera cifra de este");
+   n = Integer.parseInt(System.console().readLine());
+   
+   if (n < 10) {
+      primera = n;
+    }
    
   }
 }
