e3bbe26f61508ca1d16a83434a6c012064a553cd
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-03 19:52:26 +0100

    Ejercicio20 tema04

--- ejercicios-java/tema04/Ejercicio19.java
+++ ejercicios-java/tema04/Ejercicio19.java
@@ -6,6 +6,33 @@
 public class Ejercicio19 {
   public static void main(String[] args) {
     
+    int n = 0;
+    int digitos = 0;
+    
+    System.out.println("Por favor, introduzca un número entero (de 5 cifras como máximo): ");
+    n = Math.abs(Integer.parseInt(System.console().readLine()));
+    
+    if ( n < 10 ) {
+      digitos = 1;
+    }
+    
+    if (( n >= 10 ) && ( n < 100 )) {
+      digitos = 2;
+    }
+    
+    if (( n >= 100 ) && ( n < 1000 )) {
+      digitos = 3;
+    }
+    
+    if (( n >= 1000 ) && ( n < 10000 )) {
+      digitos = 4;
+    }
+    
+    if ( n >= 10000 ) {
+      digitos = 5;
+    }
+    
+    System.out.println("El número introducido tiene " + digitos + " dígitos.");
    
    
   }
--- ejercicios-java/tema04/Ejercicio20.java
+++ ejercicios-java/tema04/Ejercicio20.java
@@ -5,8 +5,44 @@
  */
 public class Ejercicio20 {
   public static void main(String[] args) {
+   
+   int n;
+   boolean capicua = false;
     
+   System.out.println("Introduce el número para saber si es capicuo o no: ");
+   n = Integer.parseInt(System.console().readLine());
    
-   
+   if (n < 10) {
+      capicua = true;
+    }
+    
+   if ((n >= 10) && (n < 100)) {
+      if ((n / 10) == (n % 10)) {
+        capicua = true;
+      }
+    }
+    
+   if ((n >= 100) && (n < 1000)) {
+      if ((n / 100) == (n % 10)) {
+        capicua = true;
+      }
+    }
+    
+   if ((n >= 1000) && (n < 100000)) {
+      if ((n / 1000) == (n % 10)) {
+        capicua = true;
+      }
+    }
+   if ((n >= 10000) && (n < 1000000)) {
+      if ((n / 10000) == (n % 10)) {
+        capicua = true;
+      }
+    }
+ 
+   if (capicua) { 
+    System.out.println("El número introducido es capicuo");
+   } else {
+    System.out.println("El número introducido no es capicuo");
+    }
   }
 }
