1146ba3bc42339582006106a80875c7bc56bdc83
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-03 18:52:58 +0100

    Ejercicio17 tema04

--- ejercicios-java/tema04/Ejercicio17.java
+++ ejercicios-java/tema04/Ejercicio17.java
@@ -6,7 +6,8 @@
 public class Ejercicio17 {
   public static void main(String[] args) {
     
-   
-   
+   System.out.println("Introduce un número para saber su ultima cifra");
+   int numero = Integer.parseInt(System.console().readLine());
+   System.out.println("La ultima cifra del numero introducido es: " + (numero % 10));
   }
 }
