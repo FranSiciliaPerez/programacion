1e43cb6307c9a25c30b19a4caf0cd5a24b3d6ee0
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-03 17:25:37 +0100

    Ejercicio15 tema04

--- ejercicios-java/tema04/Ejercicio15.java
+++ ejercicios-java/tema04/Ejercicio15.java
@@ -1,12 +1,34 @@
 /**
- * 
+ * Escribe un programa que pinte una pirámide rellena con un carácter introducido
+ * por teclado que podrá ser una letra, un número o un símbolo como *, +, -, $, &,
+ * etc. El programa debe permitir al usuario mediante un menú elegir si el vértice
+ * de la pirámide está apuntando hacia arriba, hacia abajo, hacia la izquierda o
+ * hacia la derecha.
  * 
  * @author Francisco J. Sicilia
  */
 public class Ejercicio15 {
   public static void main(String[] args) {
-    
-   
+    System.out.println("1- *");
+    System.out.println("2- +");
+    System.out.println("3- -");
+    System.out.println("4- $");
+    System.out.println("5- &");
+    System.out.printf("Selecciona el número o el simbolo que quiere utilzar");
+    int simbolo = Integer.parseInt();
+   switch (simbolo) {
+      case 1:
+        
+      case 2:
+      
+      case 3:
+      
+      case 4:
+      
+      case 5:
+      
+      break;
+     }
    
   }
 }
