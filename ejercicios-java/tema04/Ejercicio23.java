217d60f9b291f28938005312767abf087b795367
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-04 12:30:11 +0100

    Ejercico23 tema04

--- ejercicios-java/tema04/Ejercicio23.java
+++ ejercicios-java/tema04/Ejercicio23.java
@@ -6,7 +6,9 @@
 public class Ejercicio23 {
   public static void main(String[] args) {
     
-   
+   System.out.printf("Introduzca la base imponible: ");
+   System.out.printf("Introduzca el tipo de IVA(general, reducido o superreducido): ");
+   System.out.printf("Introduzca el código promocional: ");
    
   }
 }
