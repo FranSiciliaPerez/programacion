62d4e429b2fbc3492f67b957ce3dcbb4a1531e52
Fran Sicilia <franciscojavier.sicilia.alu@iescampanillas.com>
2021-11-08 20:40:04 +0100

    Ej2 tema05

--- ejercicios-java/tema05/Ejercicio1.java
+++ ejercicios-java/tema05/Ejercicio1.java
@@ -5,7 +5,7 @@
 public class Ejercicio1 {
     public static void main(String[] args) {
     for (int i = 0; i <= 100 ; i++) {
-    System.out.println("Los múltiplos de 5 de 0 a 100 son: " + i);
+    System.out.println("Los múltiplos de 6 de 0 a 100 son: " + i);
     }
  }
 }
