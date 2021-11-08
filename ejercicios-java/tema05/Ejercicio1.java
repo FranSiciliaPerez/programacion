9aea1de5fe487dae92db627de97fe659aaea0cd8
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-08 20:49:41 +0100

    Add files via upload

---
+++ ejercicios-java/tema05/Ejercicio1.java
@@ -0,0 +1,11 @@
+/**
+ * 
+* @author Luis José Sánchez
+*/
+public class Ejercicio1 {
+    public static void main(String[] args) {
+    for (int i = 0; i <= 100 ; i++) {
+    System.out.println("Los múltiplos de 8 de 0 a 100 son: " + i);
+    }
+ }
+}
