7a2eb23f7f164cd0db5cce1254e799d0929ff9f9
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-08 09:10:00 +0100

    Add files via upload

---
+++ ejercicios-java/tema05/Ejercicio1.java
@@ -0,0 +1,12 @@
+/**
+ * 
+* @author Luis José Sánchez
+*/
+public class Ejercicio1 {
+    public static void main(String[] args) {
+   
+    for (int i = 1; i <= 13; i++) {
+    System.out.println(i);
+    }
+ }
+}
