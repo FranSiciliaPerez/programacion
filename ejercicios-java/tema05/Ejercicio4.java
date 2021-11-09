cb0e8a30d390eecf819287d9a744e382ecd9c3ba
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-09 10:25:56 +0100

    Add files via upload

--- ejercicios-java/tema05/Ejercicio4.java
+++ ejercicios-java/tema05/Ejercicio4.java
@@ -1,7 +1,7 @@
 public class Ejercicio4 {
     public static void main(String[] args) {
         for (int n = 320 ; n >= 160; n -= 20) {
-        System.out.println(" : " + n);
+        System.out.println(n);
         }
      }
 }
---
+++ ejercicios-java/tema05/Ejercicio5.java
@@ -0,0 +1,17 @@
+/*
+ * Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizando
+ * un bucle while.
+ * @Author Francisco Sicilia Perez
+*/
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        
+        int n = 320;
+        System.out.println("Los números del 320 al 160, contando de 20 en 20 son: ");
+        while (n >= 160) {
+            System.out.println(n);
+            n -= 20;
+        }
+        
+    }
+}
