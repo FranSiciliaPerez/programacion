911b2502262738f60687920630744ace8591445f
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-09 09:00:53 +0100

    Terminados ejercicios 1, 4, 8  tema05

--- ejercicios-java/tema05/Ejercicio4.java
+++ ejercicios-java/tema05/Ejercicio4.java
@@ -1,7 +1,7 @@
 public class Ejercicio4 {
     public static void main(String[] args) {
         for (int n = 320 ; n >= 160; n -= 20) {
-        System.out.println("Los múltiplos de 8 de 0 a 100 son: " + n);
+        System.out.println(" : " + n);
         }
      }
 }
