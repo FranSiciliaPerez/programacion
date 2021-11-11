bd1ac2439bfb2e9c68574bf5961a94a5c67d14c3
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-11 08:37:09 +0100

    Add files via upload

--- ejercicios-java/tema05/Ejercicio7.java
+++ ejercicios-java/tema05/Ejercicio7.java
@@ -1,22 +1,23 @@
 public class Ejercicio7 {
     public static void main(String[] args) {
-        System.out.println("Introduce la combinación para poder acceder a la caja fuerte");
-        int n = Integer.parseInt(System.console().readLine());
+        
         int cont = 4;
+        System.out.println("Introduce la combinación para poder acceder a la caja fuerte, cuentas de " + cont +" intentos");
+        int n = Integer.parseInt(System.console().readLine());
         do {
             if (n == 1234) {
                 System.out.println("La caja fuerte se ha abiero satisfactoriamente");
                 cont = 0;
             } else {
-                System.out.println("Lo siento, esa no es la combinación");
                 cont--;
-                System.out.println("Introduce de nuevo la combinación para poder acceder a la caja fuerte");
+                System.out.println("Lo siento, esa no es la combinación");
+                System.out.println("Introduce de nuevo la combinación para poder acceder a la caja fuerte, dispones de " + cont + " intentos");
                 n = Integer.parseInt(System.console().readLine());
                 if (n == 1234) {
                     System.out.println("La caja fuerte se ha abiero satisfactoriamente");
                 } else {
-                    System.out.println("Lo siento, esa no es la combinación");
                     cont--;
+                    System.out.println("Lo siento, esa no es la combinación tiene " + cont + " intentos" );
                 }
             }
         } while (cont > 1);
