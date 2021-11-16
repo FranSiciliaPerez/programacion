effb4963bc2f65dd58899292b520d28426671a88
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-16 08:56:30 +0100

    Ejercicio14 tema05

---
+++ ejercicios-java/tema05/Ejercicio14.java
@@ -0,0 +1,11 @@
+/*
+ * Escribe un programa que pida una base y un exponente (entero positivo) y que
+ * calcule la potencia.
+ * 
+ * @author Francisco Sicilia
+*/
+public class Ejercicio14 {
+    public static void main(String[] args) {
+        
+    }
+}
