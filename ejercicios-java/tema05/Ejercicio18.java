a5da5c39dadddfcc3d63d0146ccbfaa7ef4fb5f4
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-16 11:06:00 +0100

    Ejercicio18 tema05

---
+++ ejercicios-java/tema05/Ejercicio18.java
@@ -0,0 +1,12 @@
+/* 
+ * Escribe un programa que obtenga los números enteros comprendidos entre dos
+ * números introducidos por teclado y validados como distintos, el programa debe
+ * empezar por el menor de los enteros introducidos e ir incrementando de 7 en 7.
+ * 
+*/
+public class Ejercicio18 {
+    public static void main(String[] args) {
+        System.out.printf("A continuación introduce dos números para mostrar los números comprendidos entre ellos");
+        System.out.println("");
+    }
+}
