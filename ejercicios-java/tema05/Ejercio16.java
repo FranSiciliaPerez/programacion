1714661deafbfe123a8b44f6b81c3a61e55f6b23
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-16 10:29:20 +0100

    Ejercicio 16 tema05

--- ejercicios-java/tema05/Ejercio16.java
+++ ejercicios-java/tema05/Ejercio16.java
@@ -1,10 +1,10 @@
 public class Ejercio16 {
     public static void main(String[] args) {
         System.out.print("Introduzca un número entero y le diré si es primo: ");
-        int numeroIntroducido = Integer.parseInt(System.console().readLine());
+        int n = Integer.parseInt(System.console().readLine());
         boolean esPrimo = true;
-        for (int i = 2; i < numeroIntroducido; i++) {
-            if ((numeroIntroducido % i) == 0) {
+        for (int i = 2; i < n; i++) {
+            if ((n % i) == 0) {
                 esPrimo = false;
             }
         }
