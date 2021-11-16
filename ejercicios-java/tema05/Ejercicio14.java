5d5ad97d3178e55257bdfdce27431f0b072fde55
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-16 10:04:37 +0100

    Ejercicio15 tema05

--- ejercicios-java/tema05/Ejercicio14.java
+++ ejercicios-java/tema05/Ejercicio14.java
@@ -6,6 +6,17 @@
 */
 public class Ejercicio14 {
     public static void main(String[] args) {
+        System.out.println("A continuación introduzca una base y exponente positivo para calcular su potencia");
+        System.out.printf("Introduzca la base: ");
+        int base = Integer.parseInt(System.console().readLine());
         
+        System.out.printf("Introduzca el exponente: ");
+        int expo = Integer.parseInt(System.console().readLine());
+        int potencia = 1;
+        while (expo > 0) {
+            potencia = potencia * base;
+            expo -= 1;
+        }
+        System.out.println("El resultado de la potencia es: " + potencia);
     }
 }
