50782cb075800af458aa4d67b03071bbad8ddc3d
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-18 09:17:55 +0100

    Ejercicio18 tema05 terminado

--- ejercicios-java/tema05/Ejercicio18.java
+++ ejercicios-java/tema05/Ejercicio18.java
@@ -15,17 +15,17 @@ public class Ejercicio18 {
         
         if (n1 != n2) {
             System.out.println("Los números introducidos son diferentes entre si, puede continuar");
-            System.out.println("Los comprendidos entre los numeros introducidos son: ");
+            System.out.println("Según los números introducidos estos son los números comprendidos entre ellos: ");
             while (n1 > n2) {
-                n2 +=7;
                 System.out.println(" " + n2);
+                n2 +=7;
             }
             while (n2 > n1) {
-                n1 +=7;
                 System.out.println(" " + n1);
+                n1 +=7;
             }
         } else {
             System.out.println("Los números introducidos son iguales, no e puede llevar a cabo el programa.");
-        } 
+        }
     }
 }
