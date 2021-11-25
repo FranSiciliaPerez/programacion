edec2da27ecad582f675f134cee61b37eabb8193
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-25 08:44:34 +0100

    Add files via upload

---
+++ ejercicios-java/tema05/Ejercicio 20.java
@@ -0,0 +1,8 @@
+/*
+ * Igual que el ejercicio anterior pero esta vez se debe pintar una pirámide hueca.
+*/
+public class Ejercicio 20 {
+    public static void main(String[] args) {
+        
+    }
+}
