2c8fa5d1d714524b9b242603eb9e9f5a2f137914
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-30 09:17:49 +0100

    Ejercicio 1 modificado

--- ejercicios-java/tema05/Ejercicio1.java
+++ ejercicios-java/tema05/Ejercicio1.java
@@ -1,12 +1,12 @@
 /**
  * Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for
  * 
-* @author Francisco Sicilia
-*/
+ * @author Francisco Sicilia
+ */
 public class Ejercicio1 {
     public static void main(String[] args) {
-    for (int n = 0; n <= 100  ; n += 5) {
-    System.out.println("Los múltiplos de 5 de 0 a 100 son: " + n);
+        for (int n = 0; n <= 100; n += 5) {
+            System.out.println("Los múltiplos de 5 de 0 a 100 son: " + n);
+        }
     }
- }
 }
