13e75f5abb412547e928d12daeae46cd1474bd55
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-30 10:55:09 +0100

    Add files via upload

---
+++ ejercicios-java/tema06/Ejercicio1.java
@@ -0,0 +1,10 @@
+/* 
+ * Escribe un programa que muestre la tirada de tres dados. Se debe mostrar
+ * también la suma total (los puntos que suman entre los tres dados).
+ * Author Francisco Sicilia
+*/
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        
+    }
+}
