119d454054eaed5d7ebcf8f8b75b5f61e6e08250
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-30 09:17:15 +0100

    Ejercicios 1 y 2 tema07

---
+++ ejercicios-java/tema07/Ejercicio2.java
@@ -0,0 +1,10 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        
+        String[] simbolo = {"'a'", "'x'", "", "", "'@'", "", "' '", "'x'", "'Q'", ""};
+        
+        for (int i = -1; i < 11; i++){
+            System.out.println(simbolo[i]);
+        }
+    }
+}
