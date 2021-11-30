9745f5cc4b4366a7662ff3babf20d55beba8bda0
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-30 11:01:06 +0100

    Ejercicio1 tema06 hecho

--- ejercicios-java/tema06/Ejercicio1.java
+++ ejercicios-java/tema06/Ejercicio1.java
@@ -1,10 +1,15 @@
 /* 
  * Escribe un programa que muestre la tirada de tres dados. Se debe mostrar
  * también la suma total (los puntos que suman entre los tres dados).
- * Author Francisco Sicilia
+ *
+ *  Author Francisco Sicilia
 */
 public class Ejercicio1 {
     public static void main(String[] args) {
-        
+        int dado1 = (int)(Math.random()*6) + 1;
+        int dado2 = (int)(Math.random()*6) + 1;
+        int dado3 = (int)(Math.random()*6) + 1;
+        int suma = dado1 + dado2 + dado3;
+        System.out.println("El resultado de la suma de los 3 dado es: " +suma);
     }
 }
