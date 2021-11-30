21a5228ba84ecfc5a9a2e1b402afaaeb56642997
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-11-30 09:19:31 +0100

    Ejercicio 22 tema07 comment

---
+++ ejercicios-java/tema05/Ejercicio22.java
@@ -0,0 +1,11 @@
+public class Ejercicio22 {
+    public static void main(String[] args) {
+      /*  System.out.println("Los números primos comprendidos entre 2 y 100 son:");
+        int i;
+        for (int i = 2; i <= 100; i++) {
+            int esPrimo = i 
+            /**if (is ){
+                System.out.printf(" "+i);
+        }*/
+    }
+}
