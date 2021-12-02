0815377747b2c56f4fc75cc1ce75b6a0c821157a
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-12-02 08:54:51 +0100

    Ejercicio2 tema06

---
+++ ejercicios-java/tema06/Ejercicio2.java
@@ -0,0 +1,23 @@
+/*
+ * Realiza un programa que muestre al azar el nombre de una carta de la baraja
+ * francesa. Esta baraja está dividida en cuatro palos: picas, corazones, diamantes
+ * y tréboles. Cada palo está formado por 13 cartas, de las cuales 9 cartas son
+ * numerales y 4 literales: 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K y A (que sería el
+ * 1). Para convertir un número en una cadena de caracteres podemos usar
+ * String.valueOf(n).
+*/
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        
+        int carta = (int)(Math.random()*13) + 1;
+        String numeroCart; 
+        if (carta > 10){
+            numeroCart;
+        }
+
+        switch(){
+            case "1":
+
+        }
+    }
+}
