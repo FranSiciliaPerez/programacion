75647b83090474861b19bce4e092c3181d21757e
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-12-07 22:06:02 +0100

    Add files via upload

---
+++ ejercicios-java/tema06/Ejercicio7.java
@@ -0,0 +1,12 @@
+/*
+ * Muestra 20 números enteros aleatorios entre 0 y 10 (ambos incluidos) separados
+ * por espacio
+ * 
+ * Author Francisco Sicilia
+*/
+public class Ejercicio7 {
+    public static void main(String[] args) {
+     
+        System.out.println("hola");
+    }
+}
