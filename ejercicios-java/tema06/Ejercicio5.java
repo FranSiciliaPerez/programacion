e59311d2ecb7a2ebb4ca089b95b4ba2a9b5d8c6a
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-12-09 09:28:25 +0100

    Add files via upload

--- ejercicios-java/tema06/Ejercicio5.java
+++ ejercicios-java/tema06/Ejercicio5.java
@@ -9,14 +9,18 @@ public class Ejercicio5 {
     public static void main(String[] args) {
         int media = 0;
         int max = 0;
-        int min = 0;
+        int nMedia = 0;
         for (int i = 0; i < 50; i++) {
             int numAl = (int)((Math.random()*100) + 100);
             System.out.print("" + numAl + " ");
             media += numAl;
-            min -= numAl;
+            max = numAl;
+            if (numAl > max){
+                max = numAl;
+            }
         }
-        System.out.println("La media de los números introducido es "+(media/50));
-        System.out.println("Hola "+min);
+        nMedia = media / 50;
+        System.out.println("La media de los números introducido es "+nMedia);
+        System.out.println("Hola "+max);
     }
 }
