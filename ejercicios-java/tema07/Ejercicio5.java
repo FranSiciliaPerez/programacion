ab9995693a0623884ac65d275769586af82fab43
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-12-13 17:27:46 +0100

    Add files via upload

--- ejercicios-java/tema07/Ejercicio5.java
+++ ejercicios-java/tema07/Ejercicio5.java
@@ -5,13 +5,33 @@
 */
 public class Ejercicio5 {
     public static void main(String[] args) {
-        
+
         int[] numero = new int[10];
-        System.out.println("A continucación introduce 10 números: ");
-        for (int i = 0; i < 10; i++){
+        int maximo = Integer.MIN_VALUE;
+        int minimo = Integer.MAX_VALUE;
+        int i;
+        System.out.println("Introduce 10 numeros pulsando ENTER para cada nñumero:");
+        for (i = 0; i < 10; i++) {
             numero[i] = Integer.parseInt(System.console().readLine());
+            if (numero[i] < minimo) {
+                minimo = numero[i];
+            }
+            if (numero[i] > maximo) {
+                maximo = numero[i];
+            }
         }
+        
         System.out.println();
 
+        for (i = 0; i < 10; i++) {
+            System.out.print(numero[i]);
+            if (numero[i] == maximo) {
+                System.out.print(" máximo");
+            }
+            if (numero[i] == minimo) {
+                System.out.print(" mínimo");
+            }
+            System.out.println();
+        }
     }
 }
