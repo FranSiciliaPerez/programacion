5753dca68fc37f50b4cfde215eba5d436ad046ac
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-12-13 16:56:15 +0100

    Add files via upload

--- ejercicios-java/tema07/Ejercicio4.java
+++ ejercicios-java/tema07/Ejercicio4.java
@@ -8,9 +8,15 @@
 */
 public class Ejercicio4 {
     public static void main(String[] args) {
-        int numero[i] = new int[20];
-        número 
-        cuadrado
-        cubo
+        
+        int numero[] = new int[20];
+        int cuadrado[] = new int[20];
+        int cubo[] = new int[20];
+        int i;
+        for (i = 0; i < 20; i++) {
+            numero[i] = (int)(Math.random()*101);
+            cuadrado[] = 
+        }
+
     }
 }
