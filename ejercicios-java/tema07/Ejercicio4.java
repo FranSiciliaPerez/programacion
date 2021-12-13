dc3bbb92a40cd6441f5a9f58d7d4a14f1ef75043
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2021-12-13 17:05:09 +0100

    Add files via upload

--- ejercicios-java/tema07/Ejercicio4.java
+++ ejercicios-java/tema07/Ejercicio4.java
@@ -9,14 +9,18 @@
 public class Ejercicio4 {
     public static void main(String[] args) {
         
-        int numero[] = new int[20];
-        int cuadrado[] = new int[20];
-        int cubo[] = new int[20];
+        int[] numero = new int[20];
+        int[] cuadrado = new int[20];
+        int[] cubo = new int[20];
         int i;
         for (i = 0; i < 20; i++) {
             numero[i] = (int)(Math.random()*101);
-            cuadrado[] = 
+            cuadrado[i] = numero[i] * numero[i];
+            cubo[i] = numero[i] * numero[i] * numero[i];
         }
-
+        System.out.println(" n │ n² │ n³\n─────┼───────┼─────────");
+        for (i = 0; i < 20; i++) {
+        System.out.printf("%4d │ %5d │%8d\n", numero[i], cuadrado[i], cubo[i]);
+        }
     }
 }
