98b125282e846bc4cb2aea62051c045f4d642f0d
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2022-01-10 20:32:25 +0100

    Add files via upload

--- ejercicios-java/tema07/Ejercicio2A.java
+++ ejercicios-java/tema07/Ejercicio2A.java
@@ -1,19 +1,43 @@
 public class Ejercicio2A {
     public static void main(String[] args) {
 
-        int[][] number = new int[4][6];
+        int[][] number = new int[4][5];
 
-        for (int i = 0; i < 10; i++) {
-            System.out.printf("Introduce un número: ");
-            number[i][i] = Integer.parseInt(System.console().readLine());
-        }
-        int fila, columna;
+        int fila;
+        int columna;
+
+        System.out.printf("Introduce un número: ");
+
         for (fila = 0; fila < 4; fila++) {
-            System.out.printf(" Fila " + fila);
             for (columna = 0; columna < 5; columna++) {
-                System.out.printf("%10d ", number[fila][columna]);
+                System.out.print("Fila " + fila + ", columna " + columna + ": ");
+                number[fila][columna] = Integer.parseInt(System.console().readLine());
             }
-            System.out.println();
         }
+
+        int sumaFila;
+        for (fila = 0; fila < 4; fila++) {
+            sumaFila = 0;
+            for (columna = 0; columna < 5; columna++) {
+                System.out.printf("%7d ", number[fila][columna]);
+                sumaFila += number[fila][columna];
+            }
+            System.out.printf("|%7d\n", sumaFila);
+        }
+        for (columna = 0; columna < 5; columna++) {
+            System.out.print("----------");
+        }
+        System.out.println("-----------");
+        int sumaColumna;
+        int sumaTotal = 0;
+        for (columna = 0; columna < 5; columna++) {
+            sumaColumna = 0;
+            for (fila = 0; fila < 4; fila++) {
+                sumaColumna += number[fila][columna];
+            }
+            sumaTotal += sumaColumna;
+            System.out.printf("%7d ", sumaColumna);
+        }
+        System.out.printf("|%7d ", sumaTotal);
     }
 }
