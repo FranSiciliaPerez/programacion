87384f3850d35ff0404cbd4729680e39d6fb4e1e
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2022-01-10 20:47:04 +0100

    Add files via upload

--- ejercicios-java/tema07/Ejercicio2A.java
+++ ejercicios-java/tema07/Ejercicio2A.java
@@ -1,3 +1,14 @@
+/**
+* 7.2 Arrays bidimensionales
+*
+* 2. Escribe un programa que pida 20 números enteros.
+* Estos números se deben introducir en un array de 4 filas por 5 columnas.
+* El programa mostrará las sumas parciales de filas y columnas igual que
+* si de una hoja de cálculo se tratara.
+* La suma total debe aparecer en la esquina inferior derecha.
+*
+* Francisco Sicilia Perez
+*/
 public class Ejercicio2A {
     public static void main(String[] args) {
 
