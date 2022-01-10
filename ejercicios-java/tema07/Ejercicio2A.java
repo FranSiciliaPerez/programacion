00d44ebdceb429c75d01ca230f0c2e8d6ca24884
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2022-01-10 18:21:14 +0100

    Add files via upload

--- ejercicios-java/tema07/Ejercicio2A.java
+++ ejercicios-java/tema07/Ejercicio2A.java
@@ -1,19 +1,17 @@
 public class Ejercicio2A {
     public static void main(String[] args) {
-        int[][] num = new int[4][5];
-        num[0][0] = 0;
-        num[0][1] = 30;
-        num[0][2] = 2;
-        num[0][4] = 5;
-        num[1][0] = 75;
-        num[1][4] = 0;
-        num[2][2] = -2;
-        num[2][4] = 11;
+
+        int[][] number = new int[4][6];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("Introduce un número: ");
+            number[i][i] = Integer.parseInt(System.console().readLine());
+        }
         int fila, columna;
         for (fila = 0; fila < 4; fila++) {
             System.out.printf(" Fila " + fila);
             for (columna = 0; columna < 5; columna++) {
-                System.out.printf("%10d ", num[fila][columna]);
+                System.out.printf("%10d ", number[fila][columna]);
             }
             System.out.println();
         }
