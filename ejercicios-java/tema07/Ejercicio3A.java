3e6fa9be47d1528ecdeadae40897b2a3d5720c2b
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2022-01-10 20:49:53 +0100

    Ej 3 array bidimensional

---
+++ ejercicios-java/tema07/Ejercicio3A.java
@@ -0,0 +1,14 @@
+/**
+* 7.2 Arrays bidimensionales
+*
+* 3. Modifica el programa anterior de tal forma que los números que se introducen en
+* el array se generen de forma aleatoria (valores entre 100 y 999).
+*
+*
+* Francisco Sicilia Perez
+*/
+public class Ejercicio3A {
+    public static void main(String[] args) {
+        
+    }
+}
