b0c1f206150a0588415b571993a32727db6bdf97
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2022-01-13 09:00:20 +0100

    Add files via upload

--- ejercicios-java/tema07/Ejercicio5A.java
+++ ejercicios-java/tema07/Ejercicio5A.java
@@ -11,31 +11,7 @@ public class Ejercicio5A {
     public static void main(String[] args) {
 
         int[][] numero = new int[6][10];
-
-        int maximo = Integer.MIN_VALUE;
-        int minimo = Integer.MAX_VALUE;
-        int i;
-        System.out.println("Introduce 10 numeros pulsando ENTER para cada número:");
-        for (i = 0; i < 60; i++) {
-            numero[i][i] = Integer.parseInt(System.console().readLine());
-            if (numero[i][i] < minimo) {
-                minimo = numero[i][i];
-            }
-            if (numero[i][i] > maximo) {
-                maximo = numero[i][i];
-            }
-        }
-        System.out.println();
-
-        for (i = 0; i < 10; i++) {
-            System.out.print(numero[i]);
-            if (numero[i][i] == maximo) {
-                System.out.print(" máximo");
-            }
-            if (numero[i][i] == minimo) {
-                System.out.print(" mínimo");
-            }
-            System.out.println();
-        }
+        numero[2][1] = 2;
+        System.out.println(""+ numero[][]);
     }
 }
