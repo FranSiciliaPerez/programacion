7a61187e4f9f23c718ff4c8601681793fea5cd25
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2022-02-03 11:13:30 +0100

    tema09

--- ejercicios-java/Tema09/Alumno.java
+++ ejercicios-java/Tema09/Alumno.java
@@ -4,24 +4,30 @@ public class Alumno {
     String nombre;
     String apellido;
     String telefono;
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
     String estudiosPrevios = "Bachillerato";
     int anoNacimiento;
     boolean esRepetidor;
 
     public Alumno() {
-
     }
 
-    public Alumno(int anoNacimiento, boolean esRepetidor, int numeroExpediente, String nombre, String apellido) {
+    public Alumno(int anoNacimiento, int numeroExpediente, String nombre, String apellido) {
         this.anoNacimiento = anoNacimiento;
-        this.esRepetidor = esRepetidor;
         this.numeroExpediente = numeroExpediente;
         this.nombre = nombre;
         this.apellido = apellido;
     }
 
     public void saluda() {
-        System.out.println("Hola soy" + this.nombre);
+        System.out.println("Hola soy " + this.nombre);
     }
 
     public void saluda(String intensidad) {
@@ -38,4 +44,5 @@ public class Alumno {
         }
             System.out.println(cadena);
     }
+    
 }
