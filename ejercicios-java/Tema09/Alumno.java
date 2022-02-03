3d26b66e86277da9b125a9e648f10733feceaab8
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2022-02-03 09:28:58 +0100

    tema09

--- ejercicios-java/Tema09/Alumno.java
+++ ejercicios-java/Tema09/Alumno.java
@@ -1,8 +1,10 @@
 public class Alumno {
+    
     int numeroExpediente;
     String nombre;
     String apellidoa;
     String telefono;
     String estudiosPrevios = "Bachillerato";
-
+    int añoNacimiento;
+    
 }
