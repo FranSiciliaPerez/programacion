2d2b9641238f791255d4cbc30d26add7a44097ff
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2022-02-08 08:39:20 +0100

    Ejercicio1 tema09

---
+++ ejercicios-java/Tema09/Ejercicio1.java
@@ -0,0 +1,10 @@
+/** 
+ * Ejercicio 1 POO
+ * Implementa la clase Caballo vista en un ejercicio anterior. Pruébala creando
+ * instancias y aplicándole algunos métodos.
+*/
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        
+    }
+}
